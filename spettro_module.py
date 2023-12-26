9cf955591af4d20903394927bc62b97aa5c7f18d
Federico Tota <federico.tota>
2023-12-26 15:22:25 +0100

    Inizio dell'analisi degli spettri: illustrazione dei diversi metodi di analisi, scrittura del primo metodo (picchi), creazione del modulo spettro_module contenente funzioni utili. Problemi: il fit fatica a convergere, quindi utilizzo funzione di pseudo-v; Possibile soluzione: lavora su starting points e boundaries

---
+++ spettro_module.py
@@ -0,0 +1,17 @@
+
+import numpy as np
+from scipy.special import voigt_profile
+def gaussiana(x,a,s,mu):
+    return a*np.exp(-(x-mu)**2/(2*s**2))/(s*np.sqrt(2*np.pi))
+def lorenziana(x,a,g,mu):
+    return a*(g/(2*np.pi))/((g/2)**2+(x-mu)**2)
+def pseudo_Voigt1(x,s,mu,g,p):
+    return p*np.exp(-(x-mu)**2/(2*s**2))/(s*np.sqrt(2*np.pi))+(1-p)*(g/(2*np.pi))/((g/2)**2+(x-mu)**2)
+def voigt(x,s,mu,g):
+    
+    return voigt_profile(x-mu,s,g)
+def max_boltz(x,vmax,C,mu):
+     return C*((1/(np.pi*vmax**2))**(3/2))*(4*np.pi)*((x-mu)**2)*np.exp(-(((x-mu)/vmax)**2)) #vamx^2=2kT/m
+if __name__=='__main__':
+	print('Testing my module')
+	assert 0==0#fal_body(0) == (0,0)

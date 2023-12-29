f5f5fa736850b0493e035ae03bc18f6e55220518
Federico Tota <federico.tota>
2023-12-29 11:12:22 +0100

    Inserimento starting point nel fit dei picchi, modifica modulo e implementazione di C2H2. Problema: il fit non converge in presenza di picchi sovrapposti, Sol: tentare in prima approx di eliminarli.

--- spettro_module.py
+++ spettro_module.py
@@ -7,9 +7,8 @@ def lorenziana(x,a,g,mu):
     return a*(g/(2*np.pi))/((g/2)**2+(x-mu)**2)
 def pseudo_Voigt1(x,s,mu,g,p):
     return p*np.exp(-(x-mu)**2/(2*s**2))/(s*np.sqrt(2*np.pi))+(1-p)*(g/(2*np.pi))/((g/2)**2+(x-mu)**2)
-def voigt(x,s,mu,g):
-    
-    return voigt_profile(x-mu,s,g)
+def voigt(x,a,s,mu,g):
+    return a*voigt_profile(x-mu,s,g)
 def max_boltz(x,vmax,C,mu):
      return C*((1/(np.pi*vmax**2))**(3/2))*(4*np.pi)*((x-mu)**2)*np.exp(-(((x-mu)/vmax)**2)) #vamx^2=2kT/m
 if __name__=='__main__':
